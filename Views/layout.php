b17e89ec8051f6099a5a86a9f6fa14cdca7a4b22
Evan Damas <92992183+Zenitude71@users.noreply.github.com>
2025-11-06 09:40:35 +0100

    Ajout bouton navigation

--- Views/layout.php
+++ Views/layout.php
@@ -7,16 +7,27 @@
     <link rel="stylesheet" href="/public/css/main.css">
 </head>
 <body>
-    <header class="main-header">
-        <div class="header-content">
+<header class="main-header">
+    <div class="header-content">
+        <div class="header-left">
             <img src="/Public/img/Zenless_Zone_Zero_logo.png" alt="Logo" class="header-icon">
             <div class="header-title">Zenless Zone Zero</div>
         </div>
-    </header>
 
-    <main class="content">
-        <?= $this->section('content') ?>
-    </main>
+        <nav class="main-nav">
+            <ul>
+                <li><a href="index.php">Accueil</a></li>
+                <li><a href="index.php?action=add-perso">Ajouter un perso</a></li>
+                <li><a href="index.php?action=add-perso-element">Ajouter un élément</a></li>
+                <li><a href="index.php?action=logs">Logs</a></li>
+                <li><a href="index.php?action=login">Login</a></li>
+            </ul>
+        </nav>
+    </div>
+</header>
 
+<main class="content">
+    <?= $this->section('content') ?>
+</main>
 </body>
 </html>
